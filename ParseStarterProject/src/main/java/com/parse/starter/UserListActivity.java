d6a01f1afc5a65024cbd1159748db2070512ad86
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-02-18 22:40:12 +0100

    get photo and permissions

--- ParseStarterProject/src/main/java/com/parse/starter/UserListActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/UserListActivity.java
@@ -1,9 +1,15 @@
 package com.parse.starter;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,6 +24,25 @@ import java.util.List;
 public class UserListActivity extends AppCompatActivity {
 
 
+    public void getPhoto(){
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getPhoto();
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -25,9 +50,21 @@ public class UserListActivity extends AppCompatActivity {
 
         menuInflater.inflate(R.menu.share_menu, menu);
 
-        
-
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.share){
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+            } else {
+                getPhoto();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
